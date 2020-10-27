27a5b8d7e6b35ff02c6481b6399a0bc74a127f92
reikoppu <reiko5505sas@gmail.com>
2020-10-27 18:14:44 +0900

    messagesテーブルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
+   resources :messages, only: [:index, :create]
+  end
 end
